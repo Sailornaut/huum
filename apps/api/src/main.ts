0973cc81027524bbcee99188f64a664c658d08ca
David Shoemaker <liveloveinspiration@gmail.com>
2026-04-23 16:14:41 -0400

    Allow Vercel preview origins in API CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,14 +6,42 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 
+function isAllowedOrigin(origin: string, allowedOrigins: string[]): boolean {
+  if (allowedOrigins.includes(origin)) {
+    return true;
+  }
+
+  if (/^https:\/\/huum-social-[a-z0-9-]+-sailornauts-projects\.vercel\.app$/i.test(origin)) {
+    return true;
+  }
+
+  if (/^http:\/\/localhost:\d+$/i.test(origin) || /^http:\/\/127\.0\.0\.1:\d+$/i.test(origin)) {
+    return true;
+  }
+
+  return false;
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const allowedOrigins = configService
+    .get<string>('CORS_ORIGINS', 'http://localhost:5173')
+    .split(',')
+    .map((origin) => origin.trim())
+    .filter(Boolean);
 
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: configService.get<string>('CORS_ORIGINS', 'http://localhost:5173').split(','),
+    origin: (origin, callback) => {
+      if (!origin || isAllowedOrigin(origin, allowedOrigins)) {
+        callback(null, true);
+        return;
+      }
+
+      callback(new Error(`Origin ${origin} is not allowed by CORS`));
+    },
     credentials: true,
   });
 
